f94c74576ac03c797c7812700be978be90b9e40c
NguyenMinhSon <67897715+sondeptrai2000@users.noreply.github.com>
2021-07-04 00:32:11 +0700

    cải thiện hiệu suất chat bằng cách đưa arr list chat vào accout thông tin

--- project/controller/mess.controller.js
+++ project/controller/mess.controller.js
@@ -12,42 +12,52 @@ class messtController {
     makeConnection(req, res, next) {
         let token = req.cookies.token
         let decodeAccount = jwt.verify(token, 'minhson')
-        AccountModel.findOne({ _id: decodeAccount }, { username: 1, avatar: 1 }, function(err, sender) {
-            AccountModel.findOne({ username: req.body.studentName }, { avatar: 1 }, function(err, receiver) {
+        AccountModel.findOne({ _id: decodeAccount }, { username: 1, avatar: 1, chat: 1 }, function(err, sender) {
+            AccountModel.findOne({ username: req.body.studentName }, { avatar: 1, chat: 1 }, function(err, receiver) {
+                var person1ListChat = sender.chat
+                var person2ListChat = receiver.chat
+                var check = false
+                for (let i = 0; i < person1ListChat.length; i++) {
+                    for (let u = 0; i < person2ListChat.length; u++) {
+                        if (person1ListChat[i] == person2ListChat[u]) {
+                            check = true;
+                            break;
+                        }
+                    }
+                }
+                console.log(check)
+                if (check == false) {
+                    var createConnection = {
+                        person1: sender.username,
+                        person1Ava: sender.avatar,
+                        person2: req.body.studentName,
+                        person2Ava: receiver.avatar,
+                        message: {
+                            ownermessenger: "Hệ thống",
+                            messContent: "Đã kết nối! Ấn vào để chat",
+                        }
+                    }
+                    chatModel.create(createConnection, function(err, data) {
+                        if (err) {
+                            res.json({ msg: 'có lỗi trogn khi tạo cuộc trò chuyện' });
+                        } else {
+                            AccountModel.updateOne({ username: req.body.studentName }, { $push: { chat: data._id } }, function(err, data) {
+                                if (err) {
+                                    res.json({ msg: 'Lỗi' });
+                                }
+                            })
+                            AccountModel.updateOne({ _id: decodeAccount }, { $push: { chat: data._id } }, function(err, data) {
+                                if (err) {
+                                    res.json({ msg: 'Lỗi' });
+                                }
+                            })
+                            next();
+                        }
+                    });
+                } else {
+                    next();
+                }
 
-                var condition = {
-                    person1: sender.username,
-                    person2: req.body.studentName,
-                }
-                var condition1 = {
-                    person1: req.body.studentName,
-                    person2: sender.username,
-                }
-                var createConnection = {
-                    person1: sender.username,
-                    person1Ava: sender.avatar,
-                    person2: req.body.studentName,
-                    person2Ava: receiver.avatar,
-                    message: {
-                        ownermessenger: "Hệ thống",
-                        messContent: "Đã kết nối! Ấn vào để chat",
-                    }
-                }
-                chatModel.find({ $or: [condition, condition1] }, function(err, data) {
-                    if (err) {
-                        res.json({ msg: 'error' });
-                    } else if (data.length == 0) {
-                        chatModel.create(createConnection, function(err, data) {
-                            if (err) {
-                                res.json({ msg: 'có lỗi trogn khi tạo cuộc trò chuyện' });
-                            } else {
-                                next();
-                            }
-                        });
-                    } else {
-                        next();
-                    }
-                })
             })
         })
     }
@@ -55,8 +65,8 @@ class messtController {
     chatForm(req, res) {
         let token = req.cookies.token
         let decodeAccount = jwt.verify(token, 'minhson')
-        AccountModel.findOne({ _id: decodeAccount }, { avatar: 1, username: 1 }, function(err, sender) {
-            chatModel.find({ $or: [{ person1: sender.username }, { person2: sender.username }] }, {
+        AccountModel.findOne({ _id: decodeAccount }, { avatar: 1, username: 1, chat: 1 }, function(err, sender) {
+            chatModel.find({ _id: { $in: sender.chat } }, {
                 // lấy tin nhắn cuối cùng trong mảng message
                 message: { $slice: -1 }
             }).sort({ updateTime: -1 }).exec(function(err, data1) {
@@ -100,41 +110,59 @@ class messtController {
         let token = req.cookies.token
         let decodeAccount = jwt.verify(token, 'minhson')
         var receiverName = req.body.receiver
-        AccountModel.findOne({ username: receiverName }, { username: 1, avatar: 1 }, function(err, data) {
+        AccountModel.findOne({ username: receiverName }, { username: 1, avatar: 1, chat: 1 }, function(err, data) {
             if (err) {
                 res.json({ msg: 'lỗi khi tìm kiếm user' });
             } else if (data) {
                 var receiverAva = data.avatar
-                AccountModel.findOne({ _id: decodeAccount }, { username: 1, avatar: 1 }, function(err, sender) {
+                AccountModel.findOne({ _id: decodeAccount }, { username: 1, avatar: 1, chat: 1 }, function(err, sender) {
                     var senderName = sender.username
                     var senderAva = sender.avatar
                     if (senderName != receiverName) {
-                        var createConnection = {
-                            person1: senderName,
-                            person1Ava: senderAva,
-                            person2: receiverName,
-                            person2Ava: receiverAva,
-                            message: {
-                                ownermessenger: "Hệ thống",
-                                messContent: "Đã kết nối! Ấn vào để chat",
+                        var person1ListChat = sender.chat
+                        var person2ListChat = data.chat
+                        var check = false
+                        for (let i = 0; i < person1ListChat.length; i++) {
+                            for (let u = 0; i < person2ListChat.length; u++) {
+                                if (person1ListChat[i] == person2ListChat[u]) {
+                                    check = true;
+                                    break;
+                                }
                             }
                         }
-                        chatModel.findOne({ $or: [{ person1: senderName, person2: receiverName }, { person1: receiverName, person2: senderName }] }, function(err, data) {
-                            if (err) {
-                                res.json({ msg: 'error' });
-                            } else if (!data) {
-                                chatModel.create(createConnection, function(err, data) {
-                                    if (err) {
-                                        res.json({ msg: 'có lỗi trogn khi tạo cuộc trò chuyện' });
-                                    } else {
-                                        var _idRoom = data._id
-                                        res.json({ msg: 'tạo cuộc hội thoại thành công', senderName, _idRoom, receiverName, senderAva, receiverAva });
-                                    }
-                                });
-                            } else {
-                                res.json({ msg: 'cuộc hội thoại đã được tạo', senderName, data, receiverName, senderAva, receiverAva });
+                        console.log(check)
+                        if (check == false) {
+                            var createConnection = {
+                                person1: senderName,
+                                person1Ava: senderAva,
+                                person2: receiverName,
+                                person2Ava: receiverAva,
+                                message: {
+                                    ownermessenger: "Hệ thống",
+                                    messContent: "Đã kết nối! Ấn vào để chat",
+                                }
                             }
-                        });
+                            chatModel.create(createConnection, function(err, data) {
+                                if (err) {
+                                    res.json({ msg: 'có lỗi trogn khi tạo cuộc trò chuyện' });
+                                } else {
+                                    AccountModel.updateOne({ username: receiverName }, { $push: { chat: data._id } }, function(err, data) {
+                                        if (err) {
+                                            res.json({ msg: 'Lỗi' });
+                                        }
+                                    })
+                                    AccountModel.updateOne({ _id: decodeAccount }, { $push: { chat: data._id } }, function(err, data) {
+                                        if (err) {
+                                            res.json({ msg: 'Lỗi' });
+                                        }
+                                    })
+                                    var _idRoom = data._id
+                                    res.json({ msg: 'tạo cuộc hội thoại thành công', senderName, _idRoom, receiverName, senderAva, receiverAva });
+                                }
+                            });
+                        } else {
+                            res.json({ msg: 'cuộc hội thoại đã được tạo', senderName, data, receiverName, senderAva, receiverAva });
+                        }
                     } else {
                         res.json({ msg: 'Bạn không thể tạo cuộc trò chuyện với chính mình' });
                     }
